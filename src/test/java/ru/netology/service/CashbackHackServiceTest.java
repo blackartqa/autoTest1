f7794739d936909b2bd3c45b4822260ca4f5966e
Artem Chernyshov <black_art@list.ru>
2021-10-19 22:42:08 +0300

    test created

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,34 +1,12 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldAmountLess1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int actual = 100;
-        int exception = service.remain(amount);
-        assertEquals(actual, exception);
-    }
-
-    @Test
-    public void shouldAmount1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual = 0;
-        int exception = service.remain(amount);
-        assertEquals(actual, exception);
-    }
-    @Test
-    public void shouldAmountMore1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1200;
-        int actual = 800;
-        int exception = service.remain(amount);
-        assertEquals(actual, exception);
+    public void remain() {
     }
 }
