c2ffb4d5c9216dda881c51a5e25cbaedcd7b7c5f
Artem Chernyshov <black_art@list.ru>
2021-10-19 22:48:30 +0300

    first test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,11 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldAmountLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = 100;
+        int expected = service.remain(amount);
+        assertEquals(expected,actual);
     }
 }
